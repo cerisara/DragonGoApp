3d133d3489a4270f46566ef7b1a25f7b3838b9e1
xtoff <cerisara@loria.fr>
2014-04-23 13:11:17 +0200

    cleanup before creating v1.2 for F-droid

--- src/fr/xtof54/jsgo/GoJsActivity.java
+++ src/fr/xtof54/jsgo/GoJsActivity.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
