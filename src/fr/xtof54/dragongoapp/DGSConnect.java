2c5adf9d3f253956b9073ba202137caaeb4efc5c
xtof <cerisara@loria.fr>
2016-11-22 20:39:26 +0100

    login ok

--- src/fr/xtof54/dragongoapp/DGSConnect.java
+++ src/fr/xtof54/dragongoapp/DGSConnect.java
@@ -1,13 +1,84 @@
 package fr.xtof54.dragongoapp;
 
+import org.apache.http.HttpEntity;
 import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.nio.charset.Charset;
+import org.apache.http.client.HttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.HttpResponse;
+import org.apache.http.Header;
 
 public class DGSConnect {
-    public String error;
+    public String error="no error";
+    private String server = "http://www.dragongoserver.net/";
+    private HttpClient httpclient=null;
     
+    // must be called from within an Async task
     public boolean connect() {
-        // TODO
-        return true;
+        boolean loginok=true;
+        error="no error";
+        System.out.println("start login run");
+        HttpParams httpparms = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpparms, 6000);
+        HttpConnectionParams.setSoTimeout(httpparms, 6000);
+        httpclient = new DefaultHttpClient(httpparms);
+        try {
+            try {
+                // just an anonymous ping for usage stats
+                // TODO: use it to check for updates + if enough stats, direct connection with the opponent app for live game
+                String cmd = "http://talc1.loria.fr/users/cerisara/DGSping.php?v=2";
+                System.out.println("dgsserver: login cmd "+cmd);
+                HttpGet httpget = new HttpGet(cmd);
+                httpclient.execute(httpget);
+            } catch (Exception e) {
+            }
+            // login
+            String u=DragonGoAct.main.login;
+            String p=DragonGoAct.main.password;
+            String cmd = server+"login.php?quick_mode=1&userid="+u+"&passwd="+p;
+            System.out.println("debug login cmd "+cmd);
+            HttpGet httpget = new HttpGet(cmd);
+            HttpResponse response = httpclient.execute(httpget);
+            Header[] heds = response.getAllHeaders();
+            for (Header s : heds) System.out.println("[HEADER] "+s);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                InputStream instream = entity.getContent();
+                BufferedReader fin = new BufferedReader(new InputStreamReader(instream, Charset.forName("UTF-8")));
+                for (;;) {
+                    String s = fin.readLine();
+                    if (s==null) break;
+                    System.out.println("LOGINlog "+s);
+                    if (s.contains("#Error")) {
+                        System.out.println("Error login; check credentials");
+                        error=s;
+                        loginok=false;
+                        break;
+                    }
+                }
+                fin.close();
+            } else {
+                loginok=false;
+                error="no valid server response";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            //				    if (s.contains("#Error")) logger.showMsg("Error login; check credentials");
+            System.out.println("ERROR serverdgs: "+e);
+            error=e.toString();
+            loginok=false;
+        }
+        System.out.println("end login run "+loginok);
+
+        return loginok;
     }
 
     public boolean sendmove(Game g, String move) {
