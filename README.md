bcc019c185b990ed81be529e5cade0cd38eea6af
CC <cerisara@loria.fr>
2019-01-02 11:54:34 +0100

    how to compile

--- README.md
+++ README.md
@@ -10,3 +10,30 @@ Follow below links for more information:
 
 * More information: [Webpage](http://cerisara.github.io/DragonGoApp/)
 
+How to compile
+--------------
+
+- You need a linux OS, preferably Ubuntu (text-only is enough, no GUI required).
+If you're running Windows 10, you should try and activate the Ubuntu sub-module.
+If you're running an older Windows, I recommend to install virtualbox and then install Ubuntu in Virtualbox.
+- Compilation is done with a simple bash script: **build.sh**
+You may take a look at it, it's pretty simple.
+- You need to install the android libraries **android.jar** as well as android programs **aapt**, **dex** with:
+	- sudo apt install android-sdk
+- The above command will also install the java sdk and the jarsigner program, as well as the android platform **android.jar**. At the time of writing this README, it is /usr/lib/android-sdk/platforms/android-23/android.jar, but it may change and you should double-check where is exactly this jar file, and update accordingly the variable at the start of the build.sh script.
+- At the end of the **build.sh** script, the generated apk file must be signed with a personal key that uniquely identifies you, otherwise it cannot be installed on any Android device. You must first generate this key with the following command:
+
+```	
+keytool -genkey -v -keystore $HOME/maclef.keystore -alias YOUR_ALIAS_NAME -keyalg RSA -keysize 2048 -validity 10000
+```	
+
+The variable **signername** at the start of the build.sh script defines the name the signer (YOUR_ALIAS_NAME), please set it correctly.
+You may also change the location of your key store, but you should then update accordingly the end of the build.sh script.
+Finally, the password that you have used when running the **keytool** command should be put in the **jarsigner.password** file
+so that the build.sh script can retrieve it to sign the apk.
+I've decided to put the password in a local file to avoid the risk to accidently push the password into a git repository.
+
+Note that this **buid.sh** script is independent of the android program and you can use it for your own program as long as you
+use the same simple directory structure.
+
+
--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # put your keystore password in this (uncommited) file:
 signerpass=$(cat jarsigner.password)
+signername="xtof54"
 
 export ANDBIN=/usr/lib/android-sdk/build-tools/debian/
 export ANDJAR=/usr/lib/android-sdk/platforms/android-23/android.jar
@@ -27,50 +28,5 @@ $ANDBIN/aapt package -f -M AndroidManifest.xml -I $ANDJAR -S res/ -F out/app.apk
 find assets -type f -exec $ANDBIN/aapt add -v out/app.apk {} \;
 cd out
 $ANDBIN/aapt add app.apk classes.dex
-jarsigner -verbose -keystore $HOME/maclef.keystore -storepass $signerpass -keypass $signerpass -sigalg SHA1withRSA -digestalg SHA1 app.apk xtof54
-
-exit
-
-# ############################################################
-# Old stuff below, don't look at it
-
-export ANDBIN="/home/xtof2/softs/android-sdk-linux/build-tools/19.1.0/"
-export ANDJAR="/home/xtof2/softs/android-sdk-linux/platforms/android-10/android.jar"
-export SDKLIB="/home/xtof2/softs/android-sdk-linux/tools/lib/sdklib.jar"
-
-
-# don't use this build.sh: it does not work.
-# Rather use an ANT build file with the "old" android toolchain, and jill/jack with the new one
-# Here is how to build an ant file:
-
-$ANDBIN/../../tools/android create project --target 1 --name totoproj --path . --package fr.xtof54.totoproj --activity TotoAct
-
-exit
-
-mkdir -p gen bin/classes
-
-SRCFILES=$(find src -name "*.java" | awk '{a=a" "$1}END{print a}')
-
-# $ANDBIN/aapt package -f -m --auto-add-overlay -I $ANDJAR -M AndroidManifest.xml -S res -J gen -F toto.ap_ --rename-manifest-package fr.xtof54.DragonGoApp -c en
-
-$ANDBIN/aapt package -f -m -I $ANDJAR -M AndroidManifest.xml -J gen -S res -v
-
-GENFILES=$(find gen -name "*.java" | awk '{a=a" "$1}END{print a}')
-LIBS=$(ls libs/*.jar | awk '{a=a":"$1}END{print a}')
-
-javac -bootclasspath $ANDJAR -source 1.7 -target 1.7 -cp "$ANDJAR""$LIBS" -d bin/classes $SRCFILES $GENFILES
-
-JARS=$(find $PWD/libs -name "*.jar" | awk '{a=a" "$1}END{print a}')
-
-cd bin/classes
-$ANDBIN/dx --dex --output toto.dex $JARS .
-cd ../..
-
-zip -r gen/assets.zip assets
-
-java -cp $SDKLIB com.android.sdklib.build.ApkBuilderMain toto.apk -u -z toto.ap_ -f bin/classes/toto.dex -z gen/assets.zip
-
-# jarsigner -verbose -storepass:file pasfle -tsa http://timestamp.digicert.com toto.apk dragongoapp 
-# $ANDBIN/zipalign -f 4 toto.apk dragongoapp.apk
-
+jarsigner -verbose -keystore $HOME/maclef.keystore -storepass $signerpass -keypass $signerpass -sigalg SHA1withRSA -digestalg SHA1 app.apk $signername
 
